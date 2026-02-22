fb2d0b90df0f9a051568c729bfcd52cce066646a
kunchalaphanendra <134250933+kunchalaphanendra@users.noreply.github.com>
2026-02-22 10:48:26 +0530

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
 
 const rootElement = document.getElementById('root');
@@ -14,3 +15,4 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
