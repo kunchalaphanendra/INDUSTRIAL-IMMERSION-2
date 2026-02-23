8b5518e6b98540827fb8c7e45159c211b33401ed
kunchalaphanendra <134250933+kunchalaphanendra@users.noreply.github.com>
2026-02-23 22:22:12 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [
@@ -7,8 +8,9 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': '/',
+      '@': path.resolve(__dirname, './'),
     },
   },
 });
+
 
